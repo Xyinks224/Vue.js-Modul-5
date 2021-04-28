544599bd713173be451bace874031217619dae0c
Xyinks224 <yukikitori5@gmail.com>
2021-04-28 16:20:47 +0700

    Vue.js Modul#5 ( ͡° ͜ʖ ͡°)

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Home Page</h1>
+        This is home page
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
